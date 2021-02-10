e16d23bad331e44aec0c59297486ff3e6520e65e
Val Nimirenco <valnimirenco@gmail.com>
2021-02-10 08:51:10 -0800

    connection

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+// routes to be added
+// routes to be added
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
