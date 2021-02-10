950deca411513a087f7f6c89534513bb7a5b946c
Val Nimirenco <valnimirenco@gmail.com>
2021-02-10 08:47:02 -0800

    workout schema

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        require: true,
+      },
+      name: {
+        type: String,
+        require: true,
+      },
+      duration: {
+        type: Number,
+        require: true,
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
